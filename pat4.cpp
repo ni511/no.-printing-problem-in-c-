d16a5e9824f03a1e434c6ba37041f202e106a96b
ni511 <103868519+ni511@users.noreply.github.com>
2022-06-02 01:13:55 +0530

    Update pat4.cpp

--- pat4.cpp
+++ pat4.cpp
@@ -3,14 +3,15 @@
 using namespace std;
 int main()
 {
-    int i=1,j=1,n;
+    int i=1,n;
     cout<<"Enter the value of n:\n";
     cin>>n;
     while(i<=n)
     {
-        while(j<=n)
+        int j=1;
+        while(j<=i)
         {
-            cout<<j;
+            cout<<i;
             j++;
         }
         cout<<endl;
